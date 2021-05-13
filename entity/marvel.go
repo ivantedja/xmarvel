42e293393bc36ae42906b8a2c1de38cd54ed29e9
Ivan Sebastian <ivan.sebastian@bukalapak.com>
2021-05-13 23:15:25 +0700

    Add Marvel entity

---
+++ entity/marvel.go
@@ -0,0 +1,22 @@
+package entity
+
+type CharacterCollection struct {
+	Code   uint   `json:"code"`
+	Status string `json:"status"`
+	Data   CharacterData
+}
+
+type CharacterData struct {
+	Offset  uint `json:"offset"`
+	Limit   uint `json:"limit"`
+	Total   uint `json:"total"`
+	Count   uint `json:"count"`
+	Results []Character
+}
+
+type Character struct {
+	ID          uint   `json:"id"`
+	Description string `json:"description"`
+	Name        string `json:"name"`
+	Modified    string `json:"modified"`
+}
